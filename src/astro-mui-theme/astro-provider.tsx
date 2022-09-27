fc81c2c23793fae6838bd7b852af1efa216e9f28
Brian <brian@briankiernan.com>
2022-09-27 10:22:08 -0400

    remove option of owner theme overrides

--- src/astro-mui-theme/astro-provider.tsx
+++ src/astro-mui-theme/astro-provider.tsx
@@ -7,47 +7,17 @@ import { astroTheme } from './astro-theme';
 export interface AstroThemeProviderProps {
   children: React.ReactNode;
   mode?: Mode;
-  theme?: Theme;
 }
 
 export const AstroThemeProvider: React.FC<AstroThemeProviderProps> = props => {
-  const { children, mode, theme: ownerTheme } = props;
+  const { children, mode } = props;
   const [theme, setTheme] = React.useState<Theme>(astroTheme(mode));
 
   React.useEffect(() => {
     console.log('[ASTRO THEME PROVIDER]:', theme);
   }, [theme]);
 
-  React.useEffect(() => {
-    console.log('[OWNER THEME]:', ownerTheme);
-  }, [ownerTheme]);
-
   React.useEffect(() => setTheme(astroTheme(mode)), [mode]);
-
-  // React.useEffect(() => {
-  //   if (!ownerTheme) return;
-
-  //   setTheme(astroTheme => ({
-  //     // owner override
-  //     ...astroTheme,
-  //     ...ownerTheme,
-  //     components: {
-  //       // astro override
-  //       ...ownerTheme.components,
-  //       ...astroTheme.components,
-  //     },
-  //     palette: {
-  //       // owner override
-  //       ...ownerTheme.palette,
-  //       ...astroTheme.palette,
-  //       background: {
-  //         // astro override
-  //         ...astroTheme.palette.background,
-  //         ...ownerTheme.palette.background,
-  //       },
-  //     },
-  //   }));
-  // }, [ownerTheme]);
 
   return (
     <ThemeProvider theme={theme}>
