47d9ca5b32439d6d48b8c5100faebab520679b50
Brian <brian@briankiernan.com>
2022-09-28 08:59:06 -0400

    add owner theme override option

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-
-const packageJson = require('./package.json');
+import packageJson from './package.json';
 
 export default [
   {
--- src/astro-mui-theme/astro-provider.tsx
+++ src/astro-mui-theme/astro-provider.tsx
@@ -7,13 +7,34 @@ import { astroTheme } from './astro-theme';
 export interface AstroThemeProviderProps {
   children: React.ReactNode;
   mode?: Mode;
+  theme?: Theme;
 }
 
+const handleOwnerTheme = (astro: Theme, owner: Theme): Theme => ({
+  // owner override all
+  ...astro,
+  ...owner,
+  components: {
+    // astro override all components
+    ...owner.components,
+    ...astro.components,
+  },
+});
+
 export const AstroThemeProvider: React.FC<AstroThemeProviderProps> = props => {
-  const { children, mode } = props;
+  const { children, mode, theme: ownerTheme } = props;
   const [theme, setTheme] = React.useState<Theme>(astroTheme(mode));
 
-  React.useEffect(() => setTheme(astroTheme(mode)), [mode]);
+  React.useEffect(() => console.log(theme), [theme]);
+
+  React.useEffect(() => {
+    const astro = astroTheme(mode);
+
+    setTheme(() => {
+      if (!ownerTheme) return astro;
+      return handleOwnerTheme(astro, ownerTheme);
+    });
+  }, [mode, ownerTheme]);
 
   return (
     <ThemeProvider theme={theme}>
