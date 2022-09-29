aa77ab61ff0d8920e1378eb712a9189a1be711d0
Brian <brian@briankiernan.com>
2022-09-29 09:12:41 -0400

    add astro typography overrides

--- src/astro-mui-theme/handle-owner-theme.ts
+++ src/astro-mui-theme/handle-owner-theme.ts
@@ -23,5 +23,9 @@ export const handleOwnerTheme = (
       // astro overrides
       ...astroTheme.palette,
     },
+    typography: {
+      // astro overrides
+      ...astroTheme.typography,
+    },
   };
 };
